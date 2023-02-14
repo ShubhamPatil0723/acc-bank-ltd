71565b3c588b7e07b22f28bda1e018c504c31bd9
Shubham Patil <shubhampatil0723@gmail.com>
2023-02-14 15:49:15 +0530

    Initial config

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from "express";
-// import { Router } from "./routes/index.js";
-// import { sequelizeConnect } from "./config/seqeulizeConfig.js";
+import * as Router from "./routes/index.js";
+import { sequelizeConnect } from "./config/sequelizeConfig.js";
 
 const { PORT, SEQUELIZE_CONNECTION } = process.env;
 const app = new express();
@@ -8,11 +8,11 @@ const app = new express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// app.use("/", Router);
+app.use("/", Router.transactionRouter);
 
-// if (SEQUELIZE_CONNECTION === "Y") {
-//   sequelizeConnect();
-// }
+if (SEQUELIZE_CONNECTION === "yes") {
+  sequelizeConnect();
+}
 
 app.listen(PORT, () => {
   console.log(`Listening at ${PORT}`);
