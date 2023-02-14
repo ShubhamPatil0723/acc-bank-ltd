06732d60e1f3a26fc847a12f85de94f38a15eee3
Adwait <adwaitrane26@gmail.com>
2023-02-14 15:37:57 +0530

    customer route

---
+++ routes/customerRoutes.js
@@ -0,0 +1,38 @@
+import express from "express";
+import { v4 as uuidv4 } from "uuid";
+import * as Controllers from "../controllers/index.js";
+import {
+  getCUstomerDetailsByAccountNummber,
+  postCustomerDetails,
+} from "../controllers/customerControllers/customerControllers.js";
+const customerRouter = express.Router();
+
+//routes starting with /customerDetails
+
+//for users
+// postCustomer
+customerRouter.post("/formFill", Controllers.postCustomerDetails);
+//getCustomer with ID
+customerRouter.get("/cust/:an", Controllers.getCUstomerDetailsByAccountNummber);
+
+//for admin
+
+//getCustomer
+customerRouter.get(
+  "/admin/:an",
+  Controllers.getCUstomerDetailsByAccountNummber
+);
+//deleteCustomer
+customerRouter.delete(
+  "/admin/:an",
+  Controllers.DeleteCUstomerDetailsByAccountNummber
+);
+//updateCustomer
+customerRouter.put(
+  "/admin/update/:an",
+  Controllers.UpdateCUstomerDetailsByAccountNummber
+);
+//getAllCustomer
+customerRouter.get("/adminn", Controllers.GetCUstomerDetails);
+
+export default customerRouter;
