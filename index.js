0805349c47790e5c5d6b38b0a42cd178d01d05c0
Adwait <adwaitrane26@gmail.com>
2023-02-14 15:31:35 +0530

    initial config

--- index.js
+++ index.js
@@ -2,7 +2,19 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const requiredVars = ["PORT", "NODE_ENV", "BASE_ROUTE", "SEQUELIZE_CONNECTION"];
+const requiredVars = [
+  "PORT",
+  "DEVELOPMENT_HOST",
+  "DEVELOPMENT_USER",
+  "DEVELOPMENT_PASSWORD",
+  "DEVELOPMENT_DB",
+  "DEVELOPMENT_DIALECT",
+  "BASE_ROUTE",
+  "DEVELOPMENT_MAX",
+  "DEVELOPMENT_MIN",
+  "DEVELOPMENT_ACQUIRE",
+  "DEVELOPMENT_IDLE",
+];
 const unusedEnvVar = requiredVars.filter((variable) => !process.env[variable]);
 
 if (unusedEnvVar.length) {
